94eb481304cc440806ac247a6e18ebcc108a8ae3
David Stephenson <davidstephenosn8@gmail.com>
2023-03-01 20:57:48 -0700

    some rubocop and other refinements

--- Lesson3/tictactoe.rb
+++ Lesson3/tictactoe.rb
@@ -6,8 +6,6 @@ COMPUTER_MARKER = 'O'
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                 [[1, 5, 9], [3, 5, 7]]              # diagonals
-
-
 player_wins = 0
 computer_wins = 0
 
@@ -50,12 +48,64 @@ end
 
 def joinor(arr, separator = ",", last_connector = "or")
   if arr.size == 0
-    return ''
+    ''
   elsif arr.size == 1
-    return arr[0]
-  else 
+    arr[0]
+  else
     arr[-1] = " #{last_connector} #{arr[-1]}"
     arr[0, arr.size - 1].join("#{separator} ") << arr[-1]
+  end
+end
+
+def rules
+  system "clear"
+  puts "Tic Tac Toe is played with players taking turns placing"
+  puts "pieces in a 3 by 3 grid with each player attempting to place three"
+  puts "pieces consecutively. Three pieces can be placed in the same row, same"
+  puts "column, or same diagonal to win the game. In our game, each area of the"
+  puts "grid corresponds to a number from 1 to 9"
+  puts ""
+  puts ""
+  puts "Press enter to display the grid with each area labelled with its"
+  puts "corresponding number."
+  gets
+  board = tutorial_board
+  display_board(board)
+  puts "Ok! Ready to go! Press enter to play!"
+  gets
+end
+
+def who_first_text
+  puts "Should you go first or the computer?"
+  puts "(Type me for player first, computer for computer first.)"
+  puts "Alternatively, you can type idk if choosing fills you with existential"
+  puts "dread and give the responsibility of choosing who goes first to the"
+  puts "computer"
+end
+
+def who_first?
+  who_first_text
+
+  loop do
+    response = gets.chomp
+    if response.downcase == "me"
+      return "player"
+    elsif response.downcase == "computer"
+      return "computer"
+    elsif response.downcase == "idk"
+      answer = ["player", "computer"].sample
+      return answer
+    else
+      prompt "Sorry, I didn't understand that input, what was that?"
+    end
+  end
+end
+
+def place_piece!(brd, current_player)
+  if current_player == "player"
+    player_places_piece!(brd)
+  elsif current_player == "computer"
+    computer_places_piece!(brd)
   end
 end
 
@@ -71,17 +121,25 @@ def player_places_piece!(brd)
 end
 
 def computer_places_piece!(brd)
-  win_square = check_for_win(brd)
-  threat_square = check_for_threat(brd)
+  win_square = check_for_threat(brd, COMPUTER_MARKER)
+  threat_square = check_for_threat(brd, PLAYER_MARKER)
   if win_square
     brd[win_square] = COMPUTER_MARKER
   elsif threat_square
-    brd[threat_square] = COMPUTER_MARKER   
+    brd[threat_square] = COMPUTER_MARKER
   elsif brd[5] == INITIAL_MARKER
     brd[5] = COMPUTER_MARKER
   else
     square = empty_squares(brd).sample
     brd[square] = COMPUTER_MARKER
+  end
+end
+
+def alternate_player(player)
+  if player == "computer"
+    "player"
+  else
+    "computer"
   end
 end
 
@@ -101,29 +159,17 @@ def check_for_2_in_a_row(brd, player)
         empty_space_number = num
       end
     end
-    if (2 == array.count { |number| player == brd[number] }) && array.include?(empty_space_number)
+    if array.count { |number| player == brd[number] } == 2
+      count_on = true
+    end
+    if count_on && array.include?(empty_space_number)
       return array
-    end 
-  end
-end
-
-def check_for_threat(brd)
-  threat_array = check_for_2_in_a_row(brd, PLAYER_MARKER)
-  threat_square = nil
-  if threat_array == WINNING_LINES
-    nil
-  elsif threat_array.count == 3
-    threat_array.each do |number|
-      if empty_squares(brd).include?(number)
-        threat_square = number
-      end
     end
   end
-  threat_square
 end
 
-def check_for_win(brd)
-  threat_array = check_for_2_in_a_row(brd, COMPUTER_MARKER)
+def check_for_threat(brd, marker)
+  threat_array = check_for_2_in_a_row(brd, marker)
   threat_square = nil
   if threat_array == WINNING_LINES
     nil
@@ -148,31 +194,11 @@ def detect_winner(brd)
   nil
 end
 
-def rules
-  system "clear"
-  puts "Tic Tac Toe is played with players taking turns placing their respective pieces in a 3 by 3 grid"
-  puts "with each player attempting to place three pieces consecutively. Three pieces can be placed in the"
-  puts "same row, same colomn, or same diagonal to win the game. In our game, each area of the grid corresponds"
-  puts "to a number from 1 to 9."
-  puts ""
-  puts "Press enter to display the grid with each area labelled with its correcponding number."
-  gets
-  board = tutorial_board
-  display_board(board)
-  puts "Ok! Ready to go! Press enter to play!"
-  gets
-end
-
 def detect_grand_champion(player, computer)
-  if player >= 5 
+  if player >= 5
     player_celebration
-    player_wins = 0
-    computer_wins = 0
   elsif computer >= 5
     computer_celebration
-    player_wins = 0
-    computer_wins = 0
-  else nil
   end
 end
 
@@ -192,8 +218,6 @@ def player_celebration
       sleep(0.25)
     end
   end
-  player_wins = 0
-  computer_wins = 0
 end
 
 def computer_celebration
@@ -212,22 +236,18 @@ def computer_celebration
       sleep(0.25)
     end
   end
-  player_wins = 0
-  computer_wins = 0
 end
 
 rules
 
 loop do
   board = initialize_board
+  current_player = who_first?
 
   loop do
     display_board(board)
-
-    player_places_piece!(board)
-    break if board_full?(board) || someone_won?(board)
-
-    computer_places_piece!(board)
+    place_piece!(board, current_player)
+    current_player = alternate_player(current_player)
     break if board_full?(board) || someone_won?(board)
   end
 
@@ -247,16 +267,19 @@ loop do
 
   detect_grand_champion(player_wins, computer_wins)
 
-  prompt "Number of computer wins: #{computer_wins}"
-  prompt "Number of player wins: #{player_wins}"
-  prompt "First to 5 is the GRAND CHAMPION"
-  if (computer_wins == 5 || player_wins == 5)
+  if computer_wins == 5 || player_wins == 5
     computer_wins = 0
     player_wins = 0
   end
-  prompt "Play again?"
+
+  prompt "Number of computer wins: #{computer_wins}"
+  prompt "Number of player wins: #{player_wins}"
+  prompt "First to 5 is the GRAND CHAMPION"
+  prompt "Play again? (Yes to continue, anything else to quit)"
   answer = gets.chomp
-  break if answer[0].downcase != 'y'
+  if answer[0].to_s.downcase != 'y'
+    break
+  end
 end
 
-prompt "Thanks for playing cutie"
+prompt "Ok, no worries. Thanks for playing"
